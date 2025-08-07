b40f7e7dd5ff434fcba5fdbd99e3015d69aab1f8
91741 <91741@PardhaSaradhi>
2025-08-07 10:30:50 +0530

    first commit uploading project with basic setup

---
+++ FurniERP/src/main/java/com/funrierp/FurniErpApplication.java
@@ -0,0 +1,13 @@
+package com.funrierp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurniErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurniErpApplication.class, args);
+	}
+
+}
---
+++ FurniERP/src/test/java/com/funrierp/FurniErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funrierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
